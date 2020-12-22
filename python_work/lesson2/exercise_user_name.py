39a49a900105f89e06a67417b5805b3ff0256bab
anranpaul <anranpaul@qq.com>
2020-12-22 23:24:08 +0800

    string's lesson

---
+++ python_work/lesson2/exercise_user_name.py
@@ -0,0 +1,25 @@
+message = "Hello Paul, would you like to learn some Python today"
+print(message)
+
+name = "paul"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+sentence = 'Albert Einstein once said, ' \
+        '"A person who never made a mistake never tried anything new."'
+print(sentence)
+
+famous_person = "Albert Einstein"
+motto = '"A person who never made a mistake never tried anything new."'
+full_motto = famous_person + " " + 'once said,' + " " + motto
+print(full_motto)
+
+name = "   paul   "
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+language = "\tpython\n"
+print("Which language?")
+print(language.title())
